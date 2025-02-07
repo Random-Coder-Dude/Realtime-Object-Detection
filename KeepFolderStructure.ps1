5c046a7688679434bbf07197276c6910f7ed55e9
Atharv - Prog <atvkumar10@gmail.com>
2025-02-07 14:10:44 -0500

    Remove for limelight stuff

--- KeepFolderStructure.ps1
+++
@@ -1,6 +0,0 @@
-# Find all empty directories and add a .gitkeep file to each
-Get-ChildItem -Recurse -Directory | Where-Object { $_.GetFiles().Count -eq 0 -and $_.GetDirectories().Count -eq 0 } | ForEach-Object {
-    New-Item -Path $_.FullName -Name ".gitkeep" -ItemType File -Force
-}
-
-Write-Output ".gitkeep files added to all empty folders."
